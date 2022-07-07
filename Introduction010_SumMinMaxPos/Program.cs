7f62fb0a27cf43b55830060bc7a770f207246914
Azamat <isabirovazamati@gmail.com>
2022-07-08 00:01:50 +0500

    Add Introduction 010 MinPos Sum MaxPos

---
+++ Introduction010_SumMinMaxPos/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] A = { 9, 5, 8, 6, 19 };
+int N = A.Length;
+int posMax = 0; int posMin = 0;
+int i = 0;
+while (i < N)
+{
+    if (A[i] > A[posMax])
+    {
+        posMax = i;
+    }
+    if (A[i] < A[posMin])
+    {
+        posMin = i;
+    }
+    i += 1;
+}
+int leftPos = 0;
+int rightPos = 0;
+if (posMin < posMax) { leftPos = posMin; rightPos = posMax; } else { leftPos = posMax; rightPos = posMin; }
+int k = leftPos + 1;
+int Sum = 0;
+while (k < rightPos)
+{
+    Sum += A[k];
+    k += 1;
+}
+Console.WriteLine("Максимум " + A[posMax] + " c индексом " + posMax + ", минимум " + A[posMin] + " с индексом " + posMin);
+Console.WriteLine("Сумма между позициями максимума и минимума равна, не включая концы отрезка " + Sum);
