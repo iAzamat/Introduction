ce6a960bd0d912f03946d7dae25b3716cc43aec4
Azamat <isabirovazamati@gmail.com>
2022-07-07 23:39:38 +0500

    Add Introduction 007 ScalarProduct

---
+++ Introduction007_ScalarProduct/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] firstVector = { 1, 8 };
+int[] secondVector = { 6, 3 };
+int scalarProduct = 0;
+int index = 0;
+if (firstVector.Length == secondVector.Length)
+{
+    int size = firstVector.Length;
+    while (index < size)
+    {
+        scalarProduct = scalarProduct + firstVector[index] * secondVector[index];
+        index+=1;
+    }
+    Console.WriteLine("Скалярное произведение равно " + scalarProduct);
+}
+else Console.WriteLine("Размерности векторов не совпадают, пожалуйста исправьте "); 
