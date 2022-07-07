99711ba600c0d0abeb8be033e16ffd9eee637ae3
Azamat <isabirovazamati@gmail.com>
2022-07-08 00:47:11 +0500

    Add Convertor DecTo => Oct Hex Oct

---
+++ Introduction013_DecToBinHexOct/Program.cs
@@ -0,0 +1,57 @@
+﻿static void DectoBin(int a)
+{
+    int b;
+    int c = a;
+    String temp = "";
+    while (a != 0)
+    {
+        b = a % 2;
+        temp = b + temp;
+        a = a / 2;
+    }
+    Console.Write(c + " => " + temp + "\n");
+}
+
+static void DectoOct(int a)
+{
+    int b;
+    int c = a;
+    String temp = "";
+    while (a != 0)
+    {
+        b = a % 8;
+        temp = b + temp;
+        a = a / 8;
+    }
+    Console.Write(c + " => " + temp + "\n");
+}
+
+static void DectoHex(int a)
+{
+    int b;
+    int c = a;
+    String hex = "";
+    String temp = "";
+    while (a != 0)
+    {
+        b = a % 16;
+        if (b == 10) { hex = "A"; };
+        if (b == 11) { hex = "B"; };
+        if (b == 12) { hex = "C"; };
+        if (b == 13) { hex = "D"; };
+        if (b == 14) { hex = "E"; };
+        if (b == 15) { hex = "F"; };
+        if (b >= 0 && b <= 9) { hex = b.ToString(); };
+        temp = hex + temp;
+        a = a / 16;
+    }
+    Console.Write(c + " => " + temp + "\n");
+}
+
+    DectoBin(999);
+    DectoHex(999);
+    DectoOct(999);
+
+
+
+
