5e3d88152ea153a0447227877e38221be899bb9f
Azamat <isabirovazamati@gmail.com>
2022-07-08 00:20:33 +0500

    Add Introduction 011 ArrayAVG

---
+++ Introduction011_ArrayAVG/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] A = { 9, 5, 8, 6, 19 };
+int N = A.Length;
+int i = 0;
+double Avg = 0;
+int Sum = 0;
+
+Console.WriteLine("Заданный массив");
+var str = string.Join(" ", A);
+Console.WriteLine(str);
+
+while (i < N)
+{
+    Sum += A[i];
+    i += 1;
+}
+Avg = (double)Sum / N;
+Console.WriteLine("Среднее арифметическое равно " + Avg);
