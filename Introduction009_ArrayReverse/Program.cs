a4803697a8be0e190f808f0497f9eb193e290fc4
Azamat <isabirovazamati@gmail.com>
2022-07-07 23:56:21 +0500

    Add Introduction 009 ArrayReverse

---
+++ Introduction009_ArrayReverse/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] A = { 9, 5, 8, 6, 19 };
+int N = A.Length;
+int i = 0;
+
+Console.WriteLine("Заданный массив");
+var str = string.Join(" ", A);
+Console.WriteLine(str);
+
+while (i < N / 2)
+{
+    int temp = A[i];
+    A[i] = A[N - 1 - i];
+    A[N - 1 - i] = temp;
+    i += 1;
+}
+
+Console.WriteLine("Перевернутый массив");
+var str2 = string.Join(" ", A);
+Console.WriteLine(str2);
