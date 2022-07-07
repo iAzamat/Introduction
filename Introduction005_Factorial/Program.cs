207cac251f41c882f34632ea2acde49443b3f356
Azamat <isabirovazamati@gmail.com>
2022-07-07 23:25:20 +0500

    Add Introduction 005 Factorial

---
+++ Introduction005_Factorial/Program.cs
@@ -0,0 +1,8 @@
+﻿int factorial = 1;
+Console.WriteLine("Введите число, факториал которого нужно вычислить:");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++)
+{
+    factorial = factorial * i;
+}
+Console.WriteLine("Факториал числа " + N + " равен " + factorial);
