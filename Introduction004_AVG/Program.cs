454a8a6b7f6ad68daaf93c76ca682b898dd68091
Azamat <isabirovazamati@gmail.com>
2022-07-07 23:18:54 +0500

    Add Introduction 004 AVG

---
+++ Introduction004_AVG/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите первое число: ");
+
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+
+int C = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите четвертое число: ");
+
+int D = Convert.ToInt32(Console.ReadLine());
+
+float result = (float)(A + B + C + D) / 4;
+
+Console.WriteLine("Среднее арифметическое из 4 чисел равно " + result);
