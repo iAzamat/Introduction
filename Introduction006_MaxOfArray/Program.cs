9872c84ef95c67a75a6186c76cd2c36bf65ecdd9
Azamat <isabirovazamati@gmail.com>
2022-07-07 23:32:28 +0500

    Add Introduction 006 MaxOfArray

---
+++ Introduction006_MaxOfArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] numbers = { 1, 8, 3, 2, 6 };
+int size = 5;
+int index = 0;
+int max = numbers[0];
+while (index < size)
+{
+    if (numbers[index] > max)
+    {
+        max = numbers[index];
+    }
+    index++;
+}
+Console.WriteLine("Максимальное число найдено и равно " + max);
